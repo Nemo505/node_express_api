6b61c43aa7df8e52af269710a826c29ac48ac1d1
nemo <dahliahalesia8@gmail.com>
2023-06-08 23:26:10 +0630

    mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,27 +11,40 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next)=> {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
-    };
-    res.status(201).json({
-        message: "post product request",
-        createProduct: product
+        price: req.body.price,
     });
+
+    product.save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: "post product request",
+            createProduct: product
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+   });
+
+  
 });
 
 router.get('/:productId', (req, res, next)=> {
     const id = req.params.productId;
-    if (id === 'special') {  
-        res.status(200).json({
-            message: "Special ID"
-        });
-    }else{
-        res.status(200).json({
-            message: "CorrectID"
-        });
-    }
+   Product.findById(id)
+   .exec()
+   .then(doc => {
+        console.log(doc);
+        res.status(200).json(doc);
+   })
+   .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+   });
 });
 
 router.patch('/:productId', (req, res, next)=> {
--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://'+ 
+        process.env.MONGO_ATLAS_USER+':'+
+        process.env.MONGO_ATLAS_PW+
+        '@cluster0.qnl2i0r.mongodb.net/?retryWrites=true&w=majority'      
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
